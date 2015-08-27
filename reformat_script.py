a9946d4243fb920af4c5e56450fcd05d01fae3ad
Andrea Ventura <aventura71@gmail.com>
2015-08-27 08:50:57 +0200

    now it considers multiple citations in the same period and also removes a space preceding the citation, if present. The moveCitations function has been entirely rewritten and now uses a regular expression to find citations in a period.

--- reformat_script.py
+++ reformat_script.py
@@ -2,7 +2,8 @@
 Takes a text file and move all instances of {...} immediately after the next '.'
 to run the script from the terminal type: python reformat_script.py PATH_TO_TXT_FILE > NAME_OF_OUTPUT_FILE
 '''
-import sys
+import sys, re
+
 def joinText(filename):
     f=open(filename,'r')
     joinedText=''
@@ -11,25 +12,33 @@ def joinText(filename):
     return joinedText
 
 def moveCitations(text):
+    periods=text.split('.')
     newText=''
-    i=0
-    while i < len(text):
-        if text[i] == '{' :
-            citationEnd=i+(text[i:].find('}'))+1
-            citation=text[i:citationEnd]
-            i=citationEnd
-            while i < len(text):
-                if text[i]=='.':
-                    newText=newText+text[i]+citation
-                    i+=1
-                    break
-                else:
-                    newText=newText+text[i]
-                    i+=1
-        newText=newText+text[i]
-        i+=1
+    for period in periods:
+        period = period + '.' #add the dot back!
+        #get all citations starts in decreasing order (from last to first)
+        citations=re.finditer('{[^}]*}', period) #finds every occurrence of {...} in the period.
+        spans =[]
+        for citation in citations: #copies the citation at the end of the period
+            period=period+citation.group()
+            spans.append(citation.span())
+        for span in reversed(spans): #deletes the citation from the body of the period.
+            if period[span[0]-1]==' ': #this ensures that if the {...} is preceded by a space, the space is removed from the edited text. Thus "text {citation} text."  is reformatted to "text text.{citation}" and not "text  text.{citation}"
+                span=list(span) #because tuples do not support list assignment
+                span[0]-=1
+            period=period[:span[0]]+period[span[1]:]
+        newText=newText+period
     return newText
+
+
 
 if __name__=='__main__':
     text = joinText(sys.argv[1])
-    print moveCitations(text)
+    formatted_text=moveCitations(text)
+    if len(sys.argv) > 2:
+        out_file=sys.argv[2]
+        f=open(out_file, 'w')
+        f.write(formatted_text)
+        f.close()
+    else:
+        print formatted_text
